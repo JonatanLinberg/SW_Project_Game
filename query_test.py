d2b0afc5f818b93a928c1ae9565f8fd6383f85d2
JonatanLinberg <98joli@gmail.com>
2021-12-06 15:05:07 +0100

    Added example SPARQL query to DBPedia

---
+++ query_test.py
@@ -0,0 +1,17 @@
+from stardog import Connection as SDConn
+
+q = """select distinct ?n ?b ?d ?lat ?long where {
+?p a dbo:Person.
+?p foaf:name ?n.
+?p dbo:birthYear ?b.
+?p dbo:deathYear ?d.
+?p dbo:birthPlace ?c.
+?c geo:lat ?lat.
+?c geo:long ?long.
+}"""
+
+with SDConn('sparql', endpoint='https://dbpedia.org/') as conn:
+	res = conn.select(q)
+
+for r in res['results']['bindings']:
+	print("%s, born %s, died %s\t\t%f:%f" % (r['n']['value'], r['b']['value'], r['d']['value'], float(r['long']['value']), float(r['lat']['value'])))
