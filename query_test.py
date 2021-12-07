a8a64bf5bd2e30b76b9c4c9aa80b681e41fecaee
JonatanLinberg <98joli@gmail.com>
2021-12-07 12:04:47 +0100

    Wikidata test query

--- query_test.py
+++ query_test.py
@@ -10,8 +10,18 @@ query = """select distinct ?n ?b ?d ?blat ?blong where {
 ?c geo:long ?blong.
 }"""
 
+q2 = """
+SELECT DISTINCT ?Concept WHERE {[] a ?Concept. } LIMIT 10
+"""
+
 with SDConn('sparql', endpoint='https://dbpedia.org/') as conn:
 	res = conn.select(query)
+
+with SDConn('sparql', endpoint='https://query.wikidata.org/') as conn:
+	res2 = conn.select(q2)
+
+print(res2)
+quit()
 
 for r in res['results']['bindings']:
 	print("%s, born %s, died %s\t\t%f:%f" % (r['n']['value'], r['b']['value'], r['d']['value'], float(r['blong']['value']), float(r['blat']['value'])))
