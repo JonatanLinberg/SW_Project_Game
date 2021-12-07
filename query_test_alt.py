8896d0b736267443a8e797b8ff64aa196aff00df
JonatanLinberg <98joli@gmail.com>
2021-12-07 13:00:31 +0100

    Now working with wikidata

--- query_test_alt.py
+++ query_test_alt.py
@@ -1,4 +1,4 @@
-from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 queries = ["""
 SELECT DISTINCT ?n ?b ?d ?blat ?blong WHERE {
@@ -15,14 +15,23 @@ SELECT DISTINCT ?n ?b ?d ?blat ?blong WHERE {
 SELECT DISTINCT ?Concept WHERE {[] a ?Concept. } LIMIT 10
 """]
 
-endpts = ['https://dbpedia.org/', 'https://www.wikidata.org/sparql/']
+endpts = ['https://dbpedia.org/sparql', 'https://query.wikidata.org/sparql']
 
 res = []
 
 for i in range(len(queries)):
 	conn = SPARQLWrapper(endpts[i])
 	conn.setQuery(queries[i])
+	conn.setReturnFormat(JSON)
 	try:
-		res.append(conn.query())
+		res.append(conn.query().convert())
 	except Exception as e: 
 		print(e)
+
+
+for r in res[0]['results']['bindings']:
+	print("%s, born %s, died %s\t\t%f:%f" % (r['n']['value'], r['b']['value'], r['d']['value'], float(r['blong']['value']), float(r['blat']['value'])))
+
+for r in res:
+	print("#######################################\n\n#######################################")
+	print(r)
